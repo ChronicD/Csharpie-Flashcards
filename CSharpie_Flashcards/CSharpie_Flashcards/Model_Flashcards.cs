027ace885af1bbfee95cf7e3a967537702611033
sleepysaurus <ylegaspi@gmail.com>
2014-08-15 15:31:10 +1200

    merging hack

---
+++ CSharpie_Flashcards/CSharpie_Flashcards/Model_Flashcards.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpie_Flashcards
+{
+    public class Model_Flashcards
+    {
+        public string CorrectAnswer = String.Empty;
+        List<string> StoringQuestion = new List<string>();
+        public void PrepData()
+        {
+            //DatabaseQuestions Read = new DatabaseQuestions;
+            string OneQuestion = Read.load();
+
+            //DL - when refactoring change this to a linq expression
+            string[] words = OneQuestion.Split(',');
+            CorrectAnswer = words[0];
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                StoringQuestion.Add(words[i].Replace("\"", String.Empty).Trim(' '));
+            }
+        }
+
+        public bool Compare(string answer)
+        {
+            if (answer == CorrectAnswer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+
+}
