648dc93320ef943430a0345c8b7b8f7b65fdb198
Sleepysaurus <ylegaspi@gmail.com>
2014-08-15 15:11:20 +1200

    anki view initial

---
+++ CSharpie_Flashcards/CSharpie_Flashcards/Model_Play.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpie_Flashcards
+{
+    class Model_Play
+    {
+        public List<string> _question;
+        public Model_Play()
+        {
+            _question = question;
+        }
+
+        public string Game()
+        {
+            
+        }
+    }
+}
---
+++ CSharpie_Flashcards/CSharpie_Flashcards/View_Play.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpie_Flashcards
+{
+    class View_Play
+    {
+        
+
+        public void RenderGame()
+        {
+            
+            List<string> testAns = new List<string>();
+
+            testAns.Add("Question");
+            testAns.Add("ans1");
+            testAns.Add("ans2");
+            testAns.Add("ans3");
+
+            string question = testAns[0];
+
+            Console.Clear();
+            Console.WriteLine(question);
+
+
+            for (int i = 1; i < testAns.Count; i++)
+            {
+                Console.WriteLine(Convert.ToString(i)+". "+ testAns[i]);
+            }
+
+            Console.WriteLine("Please select the number that is the correct answer or press Q  to quit.");
+
+
+        }
+    }
+}
