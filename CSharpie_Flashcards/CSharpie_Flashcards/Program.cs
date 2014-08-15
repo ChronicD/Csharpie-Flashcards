6a62fde1629f71fa06c12101f0cc42f365a41c2a
Ninjah45 <Ben.tehira@gmail.com>
2014-08-15 15:19:04 +1200

    Linden files

--- CSharpie_Flashcards/CSharpie_Flashcards/Program.cs
+++ CSharpie_Flashcards/CSharpie_Flashcards/Program.cs
@@ -8,16 +8,8 @@ namespace CSharpie_Flashcards
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-            List<string> exampleDataToView = new List<string>();
-            //{ "What is the ground speed of a migrating Swallow", "A", "African or European?", "That's easy, it's blue", "Brave Sir Robin" }
-            string[] blah = new string[] { "What is the ground speed of a migrating Swallow", "A", "African or European?", "That's easy, it's blue", "Brave Sir Robin" };
-            exampleDataToView.AddRange(blah);
-            
-            Console.WriteLine(exampleDataToView);
-		}
-
-
+        static void Main(string[] args)
+        {
+        }
 	}
 }
